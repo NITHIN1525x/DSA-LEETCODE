869a9f41661b84fa01634c02e6648b42b565e8b6
Nithin <nnitin90430@gmail.com>
2026-07-28 21:06:46 +0530

    Time: 0 ms (100%), Space: 42.4 MB (67.37%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isHappy(int n) {
+        while(n != 1 && n != 4) // n != 4 because it will be cycle 
+        {
+            int sum = 0;
+            while(n > 0)
+            {
+                int last = n % 10;
+                int square = last * last;
+                sum = sum + square;
+                n /= 10;
+            }
+            n = sum;
+        }
+        return n == 1;
+        
+    }
+}
