128c9904b6be41906a2a89d151165351a7a0bfdd
Nithin <nnitin90430@gmail.com>
2026-07-28 21:12:23 +0530

    Time: 1 ms (77.32%), Space: 42 MB (97.84%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -16,6 +16,23 @@ class Solution {
 
 
 
-        
+        // HashSet<Integer> set = new HashSet<>();
+        // while(n != 1)
+        // {
+        //     if(set.contains(n))
+        //     {
+        //         return false;
+        //     }
+        //     set.add(n);
+        //     int sum = 0;
+        //     while(n > 0)
+        //     {
+        //         int last = n % 10;
+        //         sum += (last * last);
+        //         n /= 10;
+        //     }
+        //     n = sum;
+        // }
+        // return true;
     }
 }
