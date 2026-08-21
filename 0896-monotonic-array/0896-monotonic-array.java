7253fd4a0ac052f1a919557be138785617e67a2d
Nithin <nnitin90430@gmail.com>
2026-08-21 20:54:00 +0530

    Time: 0 ms (100%), Space: 85.5 MB (7.31%) - LeetHub

---
+++ 0896-monotonic-array/0896-monotonic-array.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isMonotonic(int[] nums) {
+        return isInc(nums) || isDec(nums);
+    }
+    public boolean isInc(int[] nums)
+    {
+        for(int i=0;i<nums.length-1;i++)
+        {
+            if(nums[i] > nums[i+1])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    public boolean isDec(int[] nums)
+    {
+        for(int i=0;i<nums.length-1;i++)
+        {
+            
+            
+            if(nums[i] < nums[i+1])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
