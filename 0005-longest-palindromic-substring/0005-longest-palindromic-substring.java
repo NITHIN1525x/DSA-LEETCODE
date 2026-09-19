0ba4f87f2ff7e840eeb4df45a7fe258d59c8429b
Nithin <nnitin90430@gmail.com>
2026-09-19 19:45:26 +0530

    Time: 15 ms (60.81%), Space: 43.7 MB (59.74%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,21 +1,19 @@
 class Solution {
     int maxi = Integer.MIN_VALUE;
     int si = -1;
+
     public String longestPalindrome(String s) {
-        for(int i=0;i<s.length();i++)
-        {
-            expand(s,i,i);
-            expand(s,i,i+1);
+        for (int i = 0; i < s.length(); i++) {
+            expand(s, i, i);
+            expand(s, i, i + 1);
         }
-        return s.substring(si,si+maxi);
+        return s.substring(si, si + maxi);
     }
-    public void expand(String s,int l,int r)
-    {
-        while(l>=0 && r<s.length() && s.charAt(l) == s.charAt(r))
-        {
-            if(r-l+1 > maxi)
-            {
-                maxi = r-l+1;
+
+    public void expand(String s, int l, int r) {
+        while (l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)) {
+            if (r - l + 1 > maxi) {
+                maxi = r - l + 1;
                 si = l;
             }
             l--;
@@ -23,7 +21,3 @@ class Solution {
         }
     }
 }
-
-// Synced seamlessly with LeetHub Pro
-// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
-// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
